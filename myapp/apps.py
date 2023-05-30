0bea3b0dd51de1d913f0ba6c2a17e9c60cec2a56
Partho Debnath <parthodebnath28@gmail.com>
2023-05-31 00:02:28 +0600

    app name change in django admin panel

--- myapp/apps.py
+++ myapp/apps.py
@@ -4,3 +4,4 @@ from django.apps import AppConfig
 class MyappConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'myapp'
+    verbose_name = "Task Manager" # change the app name in admin panel
